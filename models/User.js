e50a042d845b50ccd3eb780a9d5548ad973155da
Malik Henry <mhenry30658@gmail.com>
2022-07-20 15:05:52 -0400

    created schema for users

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+const thoughtSchema = require("./Thought");
+
+const userSchema = new Schema(
+    {
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, // RegEx to check the form of a valid email
+    },
+    thoughts: {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+    },
+    friends: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    toJSON: {
+        virtuals: true,
+        getters: true,
+    },
+}
+);
+
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
