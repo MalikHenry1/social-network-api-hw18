49d9caa1ec4fa8187db79c5f18a8d80808064983
Malik Henry <mhenry30658@gmail.com>
2022-07-20 17:55:37 -0400

    built routes for userRoutes

---
+++ routes/userRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+
+const {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../controllers/user-controllers');
+
+router.route('/')
+    .get(getUsers)
+    .post(createUser);
+
+router.route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+router.route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend);
+
+
+module.exports = router;
